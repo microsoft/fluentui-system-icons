00300e66fce96c9bff82a02b1ff408243224ce54
Will Hou <wih@microsoft.com>
2020-07-12 21:39:25 -0400

    Add sample dart code output

---
+++ flutter/fluent_icons.dart
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// @dart = 2.8
+
+import 'package:flutter/widgets.dart';
+
+class FluentIcons {
+
+  // This class is not meant to be instantiated or extended; this constructor
+  // prevents instantiation and extension.
+  // ignore: unused_element
+  FluentIcons._();
+
+  // Generated code: do not hand-edit.
+  // See https://github.com/microsoft/fluentui-system-icons
+  // BEGIN GENERATED
+
+  /// fluent icon named "access time" in size 24 and regular style.
+  static const IconData access_time_24_regular = IconData(0xf101, fontFamily: 'FluentSystemIcons-Regular');
+
+  // TODO: Other icons to be generated
+
+  // END GENERATED
+}
