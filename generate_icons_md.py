2be43f799b8936170371c68b8a0566ec79a3b32f
Nick Romano <nick.r.romano@gmail.com>
2020-07-29 17:58:09 -0700

    Show all weights in icon sheet

--- generate_icons_md.py
+++ generate_icons_md.py
@@ -2,6 +2,7 @@
 # Licensed under the MIT license.
 
 import os
+from collections import defaultdict
 
 
 def to_camel_case(snake_str):
@@ -20,24 +21,30 @@ with open("icons.md", "w") as icons_md:
         if asset_dir == ".DS_Store":
             continue
 
-        largest_svg_icon_path = None
+        largest_svg_icon_path_by_weight = {}
 
-        ios_references = []
-        android_references = []
+        ios_references_by_weight = defaultdict(list)
+        android_references_by_weight = defaultdict(list)
+        weights = set()
 
         pdf_dir = os.path.join("assets", asset_dir, "PDF")
         for filename in sorted(os.listdir(pdf_dir)):
-            components = "_".join(filename.replace(".pdf", "").split("_")[2:])
-            ios_references.append(f'`{to_camel_case(components)}`')
+            components = filename.replace(".pdf", "").split("_")[2:]
+            weight = components[-1]
+            weights.add(weight)
+            ios_references_by_weight[weight].append(f'`{to_camel_case("_".join(components))}`')
 
         svg_dir = os.path.join("assets", asset_dir, "SVG")
         for filename in sorted(os.listdir(svg_dir)):
-            android_references.append(f'`{filename.replace(".svg", "")}`')
-            largest_svg_icon_path = filename
+            weight = filename.split("_")[-1].replace(".svg", "")
+            weights.add(weight)
+            android_references_by_weight[weight].append(f'`{filename.replace(".svg", "")}`')
+            largest_svg_icon_path_by_weight[weight] = filename
 
-        icons_md.write(
-            f"|{asset_dir}"
-            f'|<img src="{svg_dir}/{largest_svg_icon_path}?raw=true" width="24" height="24">'
-            f'|{"<br />".join(ios_references)}'
-            f'|{"<br />".join(android_references)}|\n'
-        )
+        for weight in sorted(weights, reverse=True):
+            icons_md.write(
+                f"|{asset_dir} ({weight})"
+                f'|<img src="{svg_dir}/{largest_svg_icon_path_by_weight[weight]}?raw=true" width="24" height="24">'
+                f'|{"<br />".join(ios_references_by_weight[weight])}'
+                f'|{"<br />".join(android_references_by_weight[weight])}|\n'
+            )
