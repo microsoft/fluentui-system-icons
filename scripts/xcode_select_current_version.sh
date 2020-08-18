1d4592bbfe15af65fd461975454c98481eff25fb
istoderi <istoderi@microsoft.com>
2020-08-18 15:51:40 -0700

    Removed unnecessary file + Added new fluent icons provided by design

--- scripts/xcode_select_current_version.sh
+++
@@ -1,3 +0,0 @@
-# Select the current officially supported version of Xcode with the naming convention used by GitHub Actions/Azure DevOps Build Pipelines
-echo "Running command: sudo xcode-select --switch '/Applications/Xcode_11.4.1.app/Contents/Developer'"
-sudo xcode-select --switch '/Applications/Xcode_11.4.1.app/Contents/Developer'
