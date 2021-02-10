4bba61e8fad5eeafe43a0f250c75b90e1b9df2ba
Spencer Nelson <spnels@microsoft.com>
2021-02-09 18:50:39 -0800

    Update the rename to not expand, so it works locally on test and add npmignore

--- packages/react-icons/template.js
+++ packages/react-icons/template.js
@@ -1,21 +1,21 @@
 function template(
-    { template },
-    opts,
-    { imports, componentName, props, jsx, exports }
+	{ template },
+	opts,
+	{ imports, componentName, props, jsx, exports }
 ) {
-    const plugins = ['jsx']
+	const plugins = ['jsx']
 	const tpl = template.smart({ plugins })
 	
 	componentName.name = componentName.name.substring(3)
 	exports.declaration.name = componentName.name
 
-    return tpl.ast`
-        ${imports}
-        function ${componentName}(${props}) {
-            return ${jsx};
-        }
-        ${exports}
-    `
+	return tpl.ast`
+		${imports}
+		function ${componentName}(${props}) {
+		return ${jsx};
+		}
+		${exports}
+	`
 }
 
 module.exports = template
