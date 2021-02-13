e33266ec9d52891bb77bff18fb984f48816ed702
Spencer Nelson <spnels@microsoft.com>
2021-02-12 17:45:15 -0800

    We're going to just export Typescript, so add an interface and a way to export it with the object name

--- packages/react-icons/template.js
+++ packages/react-icons/template.js
@@ -1,20 +1,28 @@
 function template(
 	{ template },
 	opts,
-	{ imports, componentName, props, jsx, exports }
+	{ imports, interfaces, componentName, props, jsx, exports }
 ) {
-	const plugins = ['jsx']
+	const plugins = ['jsx', 'typescript']
 	const tpl = template.smart({ plugins })
 	
 	componentName.name = componentName.name.substring(3)
+	interfaceName = 'I' + componentName.name
+	combinedExport = '{ ' + interfaceName + ', ' + componentName.name + ' }'
 	exports.declaration.name = componentName.name
 
 	return tpl.ast`
 		${imports}
-		function ${componentName}(${props}) {
+
+		interface ${interfaceName} {
+			primary?: string
+}
+
+		const ${componentName} = (props: ${interfaceName}) => {
 		return ${jsx};
 		}
-		${exports}
+		
+		export const ${combinedExport}
 	`
 }
 
