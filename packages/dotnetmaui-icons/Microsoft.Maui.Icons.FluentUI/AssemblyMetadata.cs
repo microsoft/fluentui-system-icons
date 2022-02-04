955b6c9e3ae1a7390fcd8ab40ffdf032ec6866e2
James Montemagno <james.montemagno@gmail.com>
2022-02-04 14:04:01 -0800

    Add .NET MAUI Support for font icons

---
+++ packages/dotnetmaui-icons/Microsoft.Maui.Icons.FluentUI/AssemblyMetadata.cs
@@ -0,0 +1,4 @@
+﻿using Microsoft.Maui.Icons.FluentUI;
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/2021/maui/icons/fluentui", "Microsoft.Maui.Icons.FluentUI")]
+[assembly: XmlnsPrefix("http://schemas.microsoft.com/dotnet/2021/maui/icons/fluentui", "fluentui")] 
