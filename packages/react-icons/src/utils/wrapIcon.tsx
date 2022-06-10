0edbf8e373cf3f37b806c6580919479cfc912a6c
Tomi Olubeko <ololubek@microsoft.com>
2022-06-10 14:14:43 -0700

    Update wrapIcon method to pass the proper type

--- packages/react-icons/src/utils/wrapIcon.tsx
+++ packages/react-icons/src/utils/wrapIcon.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { FluentIconsProps } from "./FluentIconsProps.types";
 import { useIconState } from "./useIconState";
 
-const wrapIcon = (Icon: () => JSX.Element, displayName?: string) => {
+const wrapIcon = (Icon: (iconProps: FluentIconsProps) => JSX.Element, displayName?: string) => {
     const WrappedIcon = (props: FluentIconsProps) => { 
         const state = useIconState(props);
         return <Icon {...state} />
