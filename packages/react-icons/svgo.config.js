b9cfcc17d62db5704fd07b1d729e7c1f36ddaf45
Tomi Olubeko <ololubek@microsoft.com>
2022-10-31 11:20:17 -0700

    (chore): Add svgo config.js file

---
+++ packages/react-icons/svgo.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            removeViewBox: false,
+            mergePaths: false        
+          },
+        },
+      },
+    ],
+  };
