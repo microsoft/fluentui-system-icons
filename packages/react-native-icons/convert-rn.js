0398c85a4e01b093867f8597f19176bf98cb311e
Warren Leung <warleu@microsoft.com>
2023-01-12 15:56:27 -0800

    spread props at the end so overrides can be passed in

--- packages/react-native-icons/convert-rn.js
+++ packages/react-native-icons/convert-rn.js
@@ -97,7 +97,7 @@ function processFolder(srcPath, destPath, resizable) {
   // See https://react-svgr.com/docs/options/ for more info
   var svgrOpts = {
     template: fileTemplate,
-    expandProps: 'start', // HTML attributes/props for things like accessibility can be passed in, and will be expanded on the svg object at the start of the object
+    expandProps: 'end', // HTML attributes/props for things like accessibility can be passed in, and will be expanded on the svg object at the start of the object
     //svgProps: { className: '{className}'}, // In order to provide styling, className will be used
     replaceAttrValues: { '#212121': '{primaryFill}' }, // We are designating primaryFill as the primary color for filling. If not provided, it defaults to null.
     typescript: true,
@@ -108,7 +108,7 @@ function processFolder(srcPath, destPath, resizable) {
 
   var svgrOptsSizedIcons = {
     template: fileTemplate,
-    expandProps: 'start', // HTML attributes/props for things like accessibility can be passed in, and will be expanded on the svg object at the start of the object
+    expandProps: 'end', // HTML attributes/props for things like accessibility can be passed in, and will be expanded on the svg object at the start of the object
     //svgProps: { className: '{className}'}, // In order to provide styling, className will be used
     replaceAttrValues: { '#212121': '{primaryFill}' }, // We are designating primaryFill as the primary color for filling. If not provided, it defaults to null.
     typescript: true,
