10ffdcf546b447b0abfa6eab3f632e96f24e1cf0
kkocdko <kkocdko@163.com>
2023-04-01 16:48:29 +0800

    Avoid modify props directly in `createFluentIcon`

--- packages/react-icons/src/utils/createFluentIcon.ts
+++ packages/react-icons/src/utils/createFluentIcon.ts
@@ -3,24 +3,24 @@ import { FluentIconsProps } from "./FluentIconsProps.types";
 import { useIconState } from "./useIconState";
 
 export type FluentIcon = {
-    (props: FluentIconsProps<React.SVGAttributes<SVGElement>>): JSX.Element;
-    displayName: string | undefined;
+    (props: FluentIconsProps): JSX.Element;
+    displayName?: string;
 }
 
 export const createFluentIcon = (displayName: string, width: string, paths: string[]): FluentIcon => {
     const viewBoxWidth = width === "1em" ? "20" : width;
     const Icon = (props: FluentIconsProps) => {
-        props = {
+        const state = {
             ...useIconState(props), // HTML attributes/props for things like accessibility can be passed in, and will be expanded on the svg object at the start of the object
             width, height: width, viewBox: `0 0 ${viewBoxWidth} ${viewBoxWidth}`, xmlns: "http://www.w3.org/2000/svg"
         };
         return React.createElement(
             "svg",
-            props,
+            state,
             ...paths.map((d) =>
                 React.createElement("path", {
                     d,
-                    fill: props.fill, // We are designating primaryFill as the primary color for filling. If not provided, it defaults to null
+                    fill: state.fill, // We are designating primaryFill as the primary color for filling. If not provided, it defaults to null
                     // key: i // The key for static children is needless 
                 })
             )
