686e5fd877a2bee4990c0cba5dce67854274db06
tomi-msft <ololubek@microsoft.com>
2023-09-15 16:38:12 -0700

    Add ref forwarding and add util exports

--- packages/react-icons/src/utils/wrapIcon.tsx
+++ packages/react-icons/src/utils/wrapIcon.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { FluentIconsProps } from "./FluentIconsProps.types";
 import { useIconState } from "./useIconState";
+import { CreateFluentIconOptions, FluentIcon } from "./createFluentIcon";
 
-const wrapIcon = (Icon: (iconProps: FluentIconsProps) => JSX.Element, displayName?: string) => {
-    const WrappedIcon = (props: FluentIconsProps) => { 
-        const state = useIconState(props);
+const wrapIcon = (Icon: (iconProps: FluentIconsProps) => JSX.Element, displayName?: string, options?: CreateFluentIconOptions) => {
+    const WrappedIcon = React.forwardRef((props: FluentIconsProps, ref: React.Ref<HTMLElement>) => { 
+        const state = {
+            ...useIconState(props, { flipInRtl: options?.flipInRtl }),
+            ref
+        };
         return <Icon {...state} />
-    }
+    }) as FluentIcon
     WrappedIcon.displayName = displayName;
     return WrappedIcon;
 }
