52e400a140eff9f0cecfd646dfdeef649beb79a2
Amber <yuanboxue@outlook.com>
2023-09-20 13:44:31 +0000

    fix

--- packages/react-icons/src/utils/FluentIconsProps.types.ts
+++ packages/react-icons/src/utils/FluentIconsProps.types.ts
@@ -1,9 +1,14 @@
 import * as React from "react";
 
-export type FluentIconsProps<TBaseAttributes extends (React.SVGAttributes<SVGElement> | React.HTMLAttributes<HTMLElement>) = React.SVGAttributes<SVGElement>> 
-= TBaseAttributes & React.RefAttributes<HTMLElement> & {
-	primaryFill?: string
-	className?: string
-	filled?: boolean
-	title?: string
-}
+export type FluentIconsProps<
+  TBaseAttributes extends
+    | React.SVGAttributes<SVGElement>
+    | React.HTMLAttributes<HTMLElement> = React.SVGAttributes<SVGElement>,
+  TRefType extends HTMLElement | SVGSVGElement = SVGSVGElement
+> = TBaseAttributes &
+  React.RefAttributes<TRefType> & {
+    primaryFill?: string;
+    className?: string;
+    filled?: boolean;
+    title?: string;
+  };
--- packages/react-icons/src/utils/wrapIcon.tsx
+++ packages/react-icons/src/utils/wrapIcon.tsx
@@ -3,16 +3,22 @@ import { FluentIconsProps } from "./FluentIconsProps.types";
 import { useIconState } from "./useIconState";
 import { CreateFluentIconOptions, FluentIcon } from "./createFluentIcon";
 
-const wrapIcon = (Icon: (iconProps: FluentIconsProps) => JSX.Element, displayName?: string, options?: CreateFluentIconOptions) => {
-    const WrappedIcon = React.forwardRef((props: FluentIconsProps, ref: React.Ref<HTMLElement>) => { 
-        const state = {
-            ...useIconState(props, { flipInRtl: options?.flipInRtl }),
-            ref
-        };
-        return <Icon {...state} />
-    }) as FluentIcon
-    WrappedIcon.displayName = displayName;
-    return WrappedIcon;
-}
+const wrapIcon = (
+  Icon: (iconProps: FluentIconsProps) => JSX.Element,
+  displayName?: string,
+  options?: CreateFluentIconOptions
+) => {
+  const WrappedIcon = React.forwardRef(
+    (props: FluentIconsProps, ref: FluentIconsProps["ref"]) => {
+      const state = {
+        ...useIconState(props, { flipInRtl: options?.flipInRtl }),
+        ref,
+      };
+      return <Icon {...state} />;
+    }
+  ) as FluentIcon;
+  WrappedIcon.displayName = displayName;
+  return WrappedIcon;
+};
 
 export default wrapIcon;
