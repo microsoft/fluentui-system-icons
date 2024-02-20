d3daeae66fffec77086235bf351eed70ca5e2f2f
Michael Loughry <miclo@microsoft.com>
2024-02-20 17:40:44 +0000

    Fix typo

--- packages/react-icons/addRexportsToIndex.js
+++ packages/react-icons/addRexportsToIndex.js
@@ -3,7 +3,7 @@ module.exports = function addRexportsToIndex(indexContents, rootPath = `.`) {
     indexContents.push(`export { default as wrapIcon } from \'${rootPath}/utils/wrapIcon\'`);
     indexContents.push(`export { default as bundleIcon } from \'${rootPath}/utils/bundleIcon\'`);
     indexContents.push(`export { createFluentIcon } from \'${rootPath}/utils/createFluentIcon\'`);
-    indexContents.push(`export { createFluentFontIcon } from \'${rootPath}./utils/fonts/createFluentFontIcon\'`);
+    indexContents.push(`export { createFluentFontIcon } from \'${rootPath}/utils/fonts/createFluentFontIcon\'`);
     indexContents.push(`export type { FluentIcon } from \'${rootPath}/utils/createFluentIcon\'`);
     indexContents.push(`export * from \'${rootPath}/utils/useIconState\'`);
     indexContents.push(`export * from \'${rootPath}/utils/constants\'`);
