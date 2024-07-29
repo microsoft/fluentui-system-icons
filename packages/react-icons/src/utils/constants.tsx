337eda022d697e9fbb170db44ed44168ca8c0ba7
Tomi Olubeko <66456876+tomi-msft@users.noreply.github.com>
2024-07-29 13:01:32 -0700

    feat: Add a font icon classname to v9 font icons (#741)

--- packages/react-icons/src/utils/constants.tsx
+++ packages/react-icons/src/utils/constants.tsx
@@ -1,3 +1,4 @@
 export const iconFilledClassName = "fui-Icon-filled";
 export const iconRegularClassName = "fui-Icon-regular";
 export const iconLightClassName = "fui-Icon-light";
+export const fontIconClassName = "fui-Icon-font"
