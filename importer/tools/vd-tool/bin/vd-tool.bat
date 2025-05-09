e8c1c191d3b81e1c93179cded4547ad2af77eab1
Spencer Nelson <spnels@microsoft.com>
2025-05-09 15:26:48 -0700

    Update Android vd-tool

--- importer/tools/vd-tool/bin/vd-tool.bat
+++ importer/tools/vd-tool/bin/vd-tool.bat
@@ -1,3 +1,19 @@
+@rem
+@rem Copyright 2015 the original author or authors.
+@rem
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@rem You may obtain a copy of the License at
+@rem
+@rem      https://www.apache.org/licenses/LICENSE-2.0
+@rem
+@rem Unless required by applicable law or agreed to in writing, software
+@rem distributed under the License is distributed on an "AS IS" BASIS,
+@rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@rem See the License for the specific language governing permissions and
+@rem limitations under the License.
+@rem
+
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
@@ -13,6 +29,9 @@ if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%..
 
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
+
 @rem Add default JVM options here. You can also use JAVA_OPTS and VD_TOOL_OPTS to pass JVM options to this script.
 set DEFAULT_JVM_OPTS=
 
@@ -21,7 +40,7 @@ if defined JAVA_HOME goto findJavaFromJavaHome
 
 set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
-if "%ERRORLEVEL%" == "0" goto init
+if "%ERRORLEVEL%" == "0" goto execute
 
 echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
@@ -35,7 +54,7 @@ goto fail
 set JAVA_HOME=%JAVA_HOME:"=%
 set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-if exist "%JAVA_EXE%" goto init
+if exist "%JAVA_EXE%" goto execute
 
 echo.
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
@@ -45,28 +64,14 @@ echo location of your Java installation.
 
 goto fail
 
-:init
-@rem Get command-line arguments, handling Windows variants
-
-if not "%OS%" == "Windows_NT" goto win9xME_args
-
-:win9xME_args
-@rem Slurp the command line arguments.
-set CMD_LINE_ARGS=
-set _SKIP=2
-
-:win9xME_args_slurp
-if "x%~1" == "x" goto execute
-
-set CMD_LINE_ARGS=%*
-
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\lib\vd-tool.jar;%APP_HOME%\lib\sdk-common-26.2.0-dev.jar;%APP_HOME%\lib\sdklib-26.2.0-dev.jar;%APP_HOME%\lib\builder-test-api-3.2.0-dev.jar;%APP_HOME%\lib\builder-model-3.2.0-dev.jar;%APP_HOME%\lib\ddmlib-26.2.0-dev.jar;%APP_HOME%\lib\bcpkix-jdk15on-1.56.jar;%APP_HOME%\lib\bcprov-jdk15on-1.56.jar;%APP_HOME%\lib\repository-26.2.0-dev.jar;%APP_HOME%\lib\layoutlib-api-26.2.0-dev.jar;%APP_HOME%\lib\dvlib-26.2.0-dev.jar;%APP_HOME%\lib\common-26.2.0-dev.jar;%APP_HOME%\lib\kotlin-stdlib-jdk8-1.2.71.jar;%APP_HOME%\lib\kotlin-reflect-1.2.0.jar;%APP_HOME%\lib\protobuf-java-3.4.0.jar;%APP_HOME%\lib\javax.inject-1.jar;%APP_HOME%\lib\gson-2.8.0.jar;%APP_HOME%\lib\commons-compress-1.12.jar;%APP_HOME%\lib\httpmime-4.5.2.jar;%APP_HOME%\lib\httpclient-4.5.2.jar;%APP_HOME%\lib\httpcore-4.4.5.jar;%APP_HOME%\lib\annotations-26.2.0-dev.jar;%APP_HOME%\lib\kxml2-2.3.0.jar;%APP_HOME%\lib\kotlin-stdlib-jdk7-1.2.71.jar;%APP_HOME%\lib\kotlin-stdlib-1.2.71.jar;%APP_HOME%\lib\annotations-13.0.jar;%APP_HOME%\lib\javax.activation-1.2.0.jar;%APP_HOME%\lib\jaxb-runtime-2.2.11.jar;%APP_HOME%\lib\jimfs-1.1.jar;%APP_HOME%\lib\guava-23.0.jar;%APP_HOME%\lib\jaxb-core-2.2.11.jar;%APP_HOME%\lib\stax-ex-1.7.7.jar;%APP_HOME%\lib\FastInfoset-1.2.13.jar;%APP_HOME%\lib\commons-logging-1.2.jar;%APP_HOME%\lib\commons-codec-1.9.jar;%APP_HOME%\lib\jaxb-api-2.2.12-b140109.1041.jar;%APP_HOME%\lib\txw2-2.2.11.jar;%APP_HOME%\lib\istack-commons-runtime-2.21.jar;%APP_HOME%\lib\kotlin-stdlib-common-1.2.71.jar;%APP_HOME%\lib\jsr305-1.3.9.jar;%APP_HOME%\lib\error_prone_annotations-2.0.18.jar;%APP_HOME%\lib\j2objc-annotations-1.1.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.14.jar
+set CLASSPATH=%APP_HOME%\lib\vd-tool.jar;%APP_HOME%\lib\sdk-common-30.1.2.jar;%APP_HOME%\lib\sdklib-30.1.2.jar;%APP_HOME%\lib\builder-test-api-7.1.2.jar;%APP_HOME%\lib\ddmlib-30.1.2.jar;%APP_HOME%\lib\layoutlib-api-30.1.2.jar;%APP_HOME%\lib\repository-30.1.2.jar;%APP_HOME%\lib\shared-30.1.2.jar;%APP_HOME%\lib\dvlib-30.1.2.jar;%APP_HOME%\lib\common-30.1.2.jar;%APP_HOME%\lib\builder-model-7.1.2.jar;%APP_HOME%\lib\annotations-30.1.2.jar;%APP_HOME%\lib\bcpkix-jdk15on-1.56.jar;%APP_HOME%\lib\bcprov-jdk15on-1.56.jar;%APP_HOME%\lib\kotlin-stdlib-jdk8-1.4.32.jar;%APP_HOME%\lib\jimfs-1.1.jar;%APP_HOME%\lib\guava-30.1-jre.jar;%APP_HOME%\lib\jaxb-runtime-2.3.2.jar;%APP_HOME%\lib\gson-2.8.6.jar;%APP_HOME%\lib\kotlin-reflect-1.4.32.jar;%APP_HOME%\lib\aapt2-proto-7.0.0-beta04-7396180.jar;%APP_HOME%\lib\protos-30.1.2.jar;%APP_HOME%\lib\protobuf-java-3.10.0.jar;%APP_HOME%\lib\javax.inject-1.jar;%APP_HOME%\lib\kxml2-2.3.0.jar;%APP_HOME%\lib\trove4j-1.0.20181211.jar;%APP_HOME%\lib\xercesImpl-2.12.0.jar;%APP_HOME%\lib\commons-compress-1.20.jar;%APP_HOME%\lib\httpmime-4.5.6.jar;%APP_HOME%\lib\httpclient-4.5.6.jar;%APP_HOME%\lib\httpcore-4.4.10.jar;%APP_HOME%\lib\kotlin-stdlib-jdk7-1.4.32.jar;%APP_HOME%\lib\kotlin-stdlib-1.4.32.jar;%APP_HOME%\lib\annotations-13.0.jar;%APP_HOME%\lib\jna-platform-5.6.0.jar;%APP_HOME%\lib\failureaccess-1.0.1.jar;%APP_HOME%\lib\listenablefuture-9999.0-empty-to-avoid-conflict-with-guava.jar;%APP_HOME%\lib\jsr305-3.0.2.jar;%APP_HOME%\lib\checker-qual-3.5.0.jar;%APP_HOME%\lib\error_prone_annotations-2.3.4.jar;%APP_HOME%\lib\j2objc-annotations-1.3.jar;%APP_HOME%\lib\stax-ex-1.8.1.jar;%APP_HOME%\lib\jakarta.xml.bind-api-2.3.2.jar;%APP_HOME%\lib\txw2-2.3.2.jar;%APP_HOME%\lib\istack-commons-runtime-3.0.8.jar;%APP_HOME%\lib\FastInfoset-1.2.16.jar;%APP_HOME%\lib\jakarta.activation-api-1.2.1.jar;%APP_HOME%\lib\xml-apis-1.4.01.jar;%APP_HOME%\lib\javax.activation-1.2.0.jar;%APP_HOME%\lib\jna-5.6.0.jar;%APP_HOME%\lib\kotlin-stdlib-common-1.4.32.jar;%APP_HOME%\lib\commons-logging-1.2.jar;%APP_HOME%\lib\commons-codec-1.10.jar
+
 
 @rem Execute vd-tool
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %VD_TOOL_OPTS%  -classpath "%CLASSPATH%" com.android.ide.common.vectordrawable.VdCommandLineTool %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %VD_TOOL_OPTS%  -classpath "%CLASSPATH%" com.android.ide.common.vectordrawable.VdCommandLineTool %*
 
 :end
 @rem End local scope for the variables with windows NT shell
