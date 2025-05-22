0636ef87b87d8b1ab257d81819bc4048e95098cf
Will <williamch@microsoft.com>
2025-05-22 15:39:00 -0700

    Color icons.

--- packages/react-icons/src/utils/bundleIcon.tsx
+++ packages/react-icons/src/utils/bundleIcon.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { iconFilledClassName, iconRegularClassName } from "./constants";
+import { iconColorClassName, iconFilledClassName, iconRegularClassName } from "./constants";
 import { makeStyles, mergeClasses } from "@griffel/react";
 import { FluentIcon } from "./createFluentIcon";
 
@@ -8,7 +8,7 @@ const useBundledIconStyles = makeStyles({
     visible: { display: "inline" }
 });
 
-const bundleIcon = (FilledIcon: FluentIcon, RegularIcon: FluentIcon) => {
+const bundleIcon = (FilledIcon: FluentIcon, RegularIcon: FluentIcon, ColorIcon: FluentIcon) => {
     const Component: FluentIcon = (props) => {
         const { className, filled, ...rest } = props;
         const styles = useBundledIconStyles();
@@ -30,6 +30,15 @@ const bundleIcon = (FilledIcon: FluentIcon, RegularIcon: FluentIcon) => {
                       !filled && styles.visible,
                       iconRegularClassName,
                       className
+                    )}
+                />
+                <ColorIcon
+                    {...rest}
+                    className={mergeClasses(
+                        styles.root,
+                        !filled && styles.visible,
+                        iconColorClassName,
+                        className
                     )}
                 />
             </React.Fragment>
--- packages/react-icons/src/utils/constants.tsx
+++ packages/react-icons/src/utils/constants.tsx
@@ -1,4 +1,5 @@
 export const iconFilledClassName = "fui-Icon-filled";
 export const iconRegularClassName = "fui-Icon-regular";
 export const iconLightClassName = "fui-Icon-light";
+export const iconColorClassName = "fui-Icon-color";
 export const fontIconClassName = "fui-Icon-font"
