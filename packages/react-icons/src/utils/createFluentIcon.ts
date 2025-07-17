46ed902a442f4e8d94b094fe71e3649f2e835629
Emma Jiang <jiangemma@microsoft.com>
2025-07-17 15:14:52 -0700

    add comment

--- packages/react-icons/src/utils/createFluentIcon.ts
+++ packages/react-icons/src/utils/createFluentIcon.ts
@@ -15,6 +15,9 @@ export type CreateFluentIconOptions = {
 
 const useRootStyles = makeStyles({
   root: {
+    // This style is required because browsers automatically have SVGs set to
+    // forced-color-adjust: preserve-parent-color, which will not override
+    // internally-defined colors in our SVG icons in high contrast mode.
     "@media (forced-colors: active)": {
       forcedColorAdjust: "auto",
     },
