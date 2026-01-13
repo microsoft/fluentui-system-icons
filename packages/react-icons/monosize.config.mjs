069074290dadb547088a9f887418599a23962352
Martin Hochel <hochelmartin@gmail.com>
2026-01-13 15:49:43 +0100

    feat: re-work local storage behaviour so monosize works as expected

--- packages/react-icons/monosize.config.mjs
+++ packages/react-icons/monosize.config.mjs
@@ -1,69 +1,66 @@
 // @ts-check
-import {join} from 'node:path'
-import {readFileSync, writeFileSync} from 'node:fs'
+import { join } from 'node:path';
+import { writeFileSync } from 'node:fs';
 import webpackBundler from 'monosize-bundler-webpack';
-
-const packageJson = JSON.parse(readFileSync(join(import.meta.dirname, 'package.json'), 'utf-8'));
-const packageName = packageJson.name;
 
 /** @type {import('monosize').MonoSizeConfig} */
 const config = {
   repository: 'https://github.com/microsoft/fluentui-system-icons',
   storage: createLocalStorage(),
-  bundler: webpackBundler(config => {
+  bundler: webpackBundler((config) => {
     config.module = config.module ?? {};
     config.module.rules = config.module.rules ?? [];
 
     // Add loader for font files
     config.module.rules.push({
-        test: /\.(ttf|woff2?|woff)$/,
-        type: 'asset/resource',
+      test: /\.(ttf|woff2?|woff)$/,
+      type: 'asset/resource',
     });
 
     return config;
   }),
-  threshold: '10kB'
+  threshold: '10kB',
 };
 
 export default config;
 
-
 /**
-  * Creates a local storage adapter for Monosize.
-  * This adapter reads and writes bundle size reports to a local JSON file.
-  * It is used to store the bundle size reports locally for the package.
-  * TODO: needs to be reworked to leverage GHA artifacts because pushing from CI to protected branch is not allowed.
+ * Creates a local storage adapter for Monosize.
+ * This adapter reads and writes bundle size reports to a local JSON file.
+ * It is used to store the bundle size reports locally for the package.
+ * TODO: needs to be reworked to leverage GHA artifacts because pushing from CI to protected branch is not allowed.
  * @returns {import('monosize').StorageAdapter}
  */
-function createLocalStorage(){
-  const storedReportPath = join(import.meta.dirname, 'monosize-baseline.json');
-
-/**
- *
- * @param {string} reportPath
- * @returns {import('monosize').BundleSizeReport}
- */
-  function getReport(reportPath){
-    return JSON.parse(readFileSync(reportPath, 'utf-8'))
-  }
+function createLocalStorage() {
+  const storageFileName = 'monosize.baseline.json';
 
   return {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    getRemoteReport: async (_branch) => {
-      const reportPath = storedReportPath;
-      const report = getReport(reportPath);
-      return {
-        commitSHA: 'local',
-        remoteReport: report
+    getRemoteReport: async (branch) => {
+      const url = `https://raw.githubusercontent.com/microsoft/fluentui-system-icons/${branch}/packages/react-icons/${storageFileName}`;
+
+      const emptyReport = { commitSHA: 'initial', remoteReport: [] };
+
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          console.log(`Failed to fetch baseline from ${url}: ${response.statusText}`);
+          return emptyReport;
+        }
+        /** @type {{branch: string; commitSHA: string; report: import('monosize').BundleSizeReport}}} */
+        const data = await response.json();
+
+        return { commitSHA: data.commitSHA, remoteReport: data.report };
+      } catch (err) {
+        const message = err instanceof Error ? err.message : String(err);
+        console.log(`Error fetching baseline: ${message}`);
+        return emptyReport;
       }
     },
-    uploadReportToRemote: async () => {
-      const reportPath = join(import.meta.dirname, 'dist/bundle-size/monosize.json');
-      const report = getReport(reportPath);
-      report.forEach(entry => {
-        entry.packageName = packageName;
-      });
-      writeFileSync(storedReportPath, JSON.stringify(report, null, 2), 'utf-8');
+    uploadReportToRemote: async (branch, commitSHA, localReport) => {
+      const storageData = { branch, commitSHA, report: localReport };
+      const storedReportPath = join(import.meta.dirname, storageFileName);
+
+      writeFileSync(storedReportPath, JSON.stringify(storageData, null, 2), 'utf-8');
     },
-  }
+  };
 }
