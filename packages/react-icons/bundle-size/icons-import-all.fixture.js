b1cadcaab32e795b3f152da484799fc11725ed19
Martin Hochel <martinhochel@microsoft.com>
2026-01-14 16:04:38 +0100

    chore(react-icons): add bundle-size metrics,gates (#877)
    * chore(react-icons): add bundle-size safe guards
    
    * ci: add bundle size infra to PR workflow
    
    * ci: add update baseline workflow
    
    * feat: re-work local storage behaviour so monosize works as expected
    
    * chore(react-icons): add atomic import bundle size fixture
    
    * chore: update codeowners
    
    * chore: fix corrupted lock file
    
    * reset lock file
    
    * update lock file after reset
    
    * chore(react-icons): setup build task caching
    
    * chore: move monosize config and packages to monorepo root
    
    * ci: udpate baseline workflow

---
+++ packages/react-icons/bundle-size/icons-import-all.fixture.js
@@ -0,0 +1,5 @@
+import * as FluentIcons from '@fluentui/react-icons';
+
+console.log(FluentIcons.AirplaneRegular);
+
+export default { name: 'Single Icon via Import all' };
---
+++ packages/react-icons/bundle-size/provider.fixture.js
@@ -0,0 +1,5 @@
+import * as Providers from '@fluentui/react-icons/providers';
+
+console.log(Providers);
+
+export default { name: 'Provider' }
