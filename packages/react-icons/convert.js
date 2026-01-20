b631d1aa7a88d64c29df8a0fea500298e4efd839
Martin Hochel <hochelmartin@gmail.com>
2026-01-20 18:35:53 +0100

    chore(react-icons): update generators to relfect bundleIcon default->named export change

--- packages/react-icons/convert.js
+++ packages/react-icons/convert.js
@@ -97,7 +97,7 @@ function processPerChunk(sourceFiles, dest, rtlMetadata) {
   const indexPath = path.join(dest, 'index.tsx');
   // Finally add the interface definition and then write out the index.
   indexContents.push("export { default as wrapIcon } from './utils/wrapIcon'");
-  indexContents.push("export { default as bundleIcon } from './utils/bundleIcon'");
+  indexContents.push("export { bundleIcon } from './utils/bundleIcon'");
   indexContents.push("export { createFluentIcon } from './utils/createFluentIcon'");
   indexContents.push("export * from './utils/useIconState'");
   indexContents.push("export * from './utils/constants'");
