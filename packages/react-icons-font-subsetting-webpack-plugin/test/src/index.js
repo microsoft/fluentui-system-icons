ffe50448d761646488c3996ae13c1584b8871811
Martin Hochel <hochelmartin@gmail.com>
2026-01-23 16:33:51 +0100

    feat(react-icons-font-subsetting-webpack-plugin): add react-icons atomic imports support

--- packages/react-icons-font-subsetting-webpack-plugin/test/src/index.js
+++ packages/react-icons-font-subsetting-webpack-plugin/test/src/index.js
@@ -1,5 +1,7 @@
+// @ts-check
 import {XboxConsole24Filled, BoardGames20Regular, GamesFilled} from '@fluentui/react-icons';
 
+console.log('chunk fonts loaded');
 console.dir({
     XboxConsole24Filled,
     BoardGames20Regular,
