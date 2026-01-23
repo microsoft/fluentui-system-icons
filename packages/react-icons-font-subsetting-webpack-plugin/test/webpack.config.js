a9a082fbec950447384c585c06e5921487c1a40f
Martin Hochel <hochelmartin@gmail.com>
2026-01-23 16:05:06 +0100

    chore(react-icons-font-subsetting-webpack-plugin): add dev target

--- packages/react-icons-font-subsetting-webpack-plugin/test/webpack.config.js
+++ packages/react-icons-font-subsetting-webpack-plugin/test/webpack.config.js
@@ -1,10 +1,22 @@
-const {default: FluentUIReactIconsFontSubsettingPlugin} = require('../lib/');
+// @ts-check
 const {resolve} = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const {default: FluentUIReactIconsFontSubsettingPlugin} = require('../lib/');
 
 const FONT_THRESHOLD_SIZE = 2 * 1_024; // 2kb
 
+const isDevServer = process.env.WEBPACK_SERVE === 'true';
+
+console.log(`Running webpack in ${isDevServer ? 'development' : 'production'} mode`);
+
+/** @type {import('webpack').Configuration} */
 module.exports = {
     context: __dirname,
+    // FluentUIReactIconsFontSubsettingPlugin requires `optimization.usedExports: true` to determine
+    // which icon exports are actually used. This is enabled by default in production mode.
+    // In development mode, webpack doesn't track used exports, so the plugin skips subsetting.
     mode: 'production',
     module: {
         rules: [
@@ -25,10 +37,13 @@ module.exports = {
         conditionNames: ['fluentIconFont', 'import']
     },
     plugins: [
+        ...(isDevServer ? [new HtmlWebpackPlugin({
+            title: 'FluentUIReactIconsFontSubsettingPlugin Demo'
+        })] : []),
         new FluentUIReactIconsFontSubsettingPlugin(),
         {
             apply(compiler) {
-                compiler.hooks.afterEmit.tap('test-subsetting', (compilation) => {    
+                compiler.hooks.afterEmit.tap('test-subsetting', (compilation) => {
                     for (const [assetName, source] of Object.entries(compilation.assets)) {
                         if (/\.(ttf|woff2?)$/.test(assetName) && source.size() > FONT_THRESHOLD_SIZE) {
                             throw new Error(`Asset "${assetName}" does not appear to have been properly subset.`)
