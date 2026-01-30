59fcceb1f3ea5d20a54f311888fc49334e5cd942
Rachel Yoo <57475719+rachethecreator@users.noreply.github.com>
2026-01-30 11:46:49 -0500

    add new and updated icons (#967)
    New
    Board / 32, 48 / Filled & Regular
    Certificate Off / 16, 20, 24 / Filled & Regular
    Document Text Extract / 16, 28, 32, 48 / Filled & Regular
    Markdown / 24 / Filled & Regular
    Medal / 16, 20, 24 / Filled & Regular
    Presenter / 16, 28, 32, 48 / Filled & Light & Regular
    Window New Rotate 180 / 16, 20, 24, 28, 32, 48 / Filled & Regular
    Window New / 28, 32, 48 / Filled & Regular
    
    Updated
    Board / 24 / Filled & Regular
    Gauge / 16 / Filled & Regular
    Presenter / 24 / Filled & Regular
    Document Add / 48 / Filled
    Document Arrow Left / 48 / Filled
    Document Lightning / 48 / Filled
    Document Lock / 48 / Filled
    Document Mention / 48 / Filled
    Document Print / 48 / Filled
    Document Ribbon / 48 / Filled
    Document Signature / 48 / Filled
    Document Text / 48 / Filled
    Settings Cog Multiple / 20, 24 / Filled & Regular
    Window New / 16, 20, 24 / Filled & Regular

--- packages/react-icons/build-verify.test.js
+++ packages/react-icons/build-verify.test.js
@@ -1830,15 +1830,15 @@ describe('Build Verification', () => {
       const { svgPathCjs, svgPathEsm } = getAssetPaths();
       const esmStats = await getStats(svgPathEsm);
       const cjsStats = await getStats(svgPathCjs);
-      expect(esmStats.jsFiles.length).toMatchInlineSnapshot(`2802`);
-      expect(cjsStats.jsFiles.length).toMatchInlineSnapshot(`2802`);
+      expect(esmStats.jsFiles.length).toMatchInlineSnapshot(`2805`);
+      expect(cjsStats.jsFiles.length).toMatchInlineSnapshot(`2805`);
     });
     it(`should have same number of atoms/fonts icon files in lib and lib-cjs`, async () => {
       const { fontsPathCjs, fontsPathEsm } = getAssetPaths();
       const esmStats = await getStats(fontsPathEsm);
       const cjsStats = await getStats(fontsPathCjs);
-      expect(esmStats.jsFiles.length).toMatchInlineSnapshot(`2795`);
-      expect(cjsStats.jsFiles.length).toMatchInlineSnapshot(`2795`);
+      expect(esmStats.jsFiles.length).toMatchInlineSnapshot(`2798`);
+      expect(cjsStats.jsFiles.length).toMatchInlineSnapshot(`2798`);
     });
     it.each(['lib', 'lib-cjs'])('should have atoms/svg directory with icon files in %s', async (libDir) => {
       const atomsSvgPath = path.join(__dirname, libDir, 'atoms', 'svg');
