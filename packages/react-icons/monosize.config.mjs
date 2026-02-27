1bfc92e60a772c45b663e0008ddd4f038c9e6235
Martin Hochel <hochelmartin@gmail.com>
2026-02-27 09:12:16 +0100

    chore(react-icons): add subsetting plugins to monosize

--- packages/react-icons/monosize.config.mjs
+++ packages/react-icons/monosize.config.mjs
@@ -1,14 +1,29 @@
 // @ts-check
 
+import { createRequire } from 'node:module';
+
 import webpackBundler from 'monosize-bundler-webpack';
 
 import { GriffelCSSExtractionPlugin } from '@griffel/webpack-extraction-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
+const require = createRequire(import.meta.url);
+const {
+  default: FluentUIReactIconsFontSubsettingPlugin,
+} = require('@fluentui/react-icons-font-subsetting-webpack-plugin');
+const {
+  default: FluentUIReactIconsSvgSpriteSubsettingPlugin,
+} = require('@fluentui/react-icons-svg-sprite-subsetting-webpack-plugin');
+
+
 import baseConfig from '../../monosize.config.mjs';
 
 const enableGriffelExtraction = process.env.GRIFFEL_EXTRACTION === 'true';
+const enableFontSubsetting = process.env.FONT_SUBSETTING === 'true';
+const enableSvgSubsetting = process.env.SVG_SUBSETTING === 'true';
 
+console.info(`Font Subsetting is ${enableFontSubsetting ? 'enabled' : 'disabled'}.`);
+console.info(`Svg Subsetting is ${enableSvgSubsetting ? 'enabled' : 'disabled'}.`);
 console.info(`Griffel CSS Extraction is ${enableGriffelExtraction ? 'enabled' : 'disabled'}.`);
 if (!enableGriffelExtraction) {
   console.info(`Set GRIFFEL_EXTRACTION=true to enable.`);
@@ -49,6 +64,13 @@ const config = {
 
     if (enableGriffelExtraction) {
       applyGriffelExtraction(config);
+    }
+
+    if (enableFontSubsetting) {
+      config.plugins.push(new FluentUIReactIconsFontSubsettingPlugin());
+    }
+    if (enableSvgSubsetting) {
+      config.plugins.push(new FluentUIReactIconsSvgSpriteSubsettingPlugin());
     }
 
     // Add loader for font files
