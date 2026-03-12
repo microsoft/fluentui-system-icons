d86ae2f6962e4b7502e975136ce067828a083505
Martin Hochel <martinhochel@microsoft.com>
2026-03-12 18:58:05 +0100

    feat(react-icons): enable griffel `makeStaticStyles` AOT for font icons (#1006)

--- packages/react-icons/monosize.config.mjs
+++ packages/react-icons/monosize.config.mjs
@@ -2,14 +2,54 @@
 
 import webpackBundler from 'monosize-bundler-webpack';
 
+import { GriffelCSSExtractionPlugin } from '@griffel/webpack-extraction-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
 import baseConfig from '../../monosize.config.mjs';
+
+const enableGriffelExtraction = process.env.GRIFFEL_EXTRACTION === 'true';
+
+console.info(`Griffel CSS Extraction is ${enableGriffelExtraction ? 'enabled' : 'disabled'}.`);
+if (!enableGriffelExtraction) {
+  console.info(`Set GRIFFEL_EXTRACTION=true to enable.`);
+}
+
+/**
+ * @param {import('webpack').Configuration} config
+ */
+function applyGriffelExtraction(config) {
+  /** @typedef {{ module: {rules: Array<import('webpack').RuleSetRule>}; plugins: Array<import('webpack').WebpackPluginInstance> }} AssertConfig */
+
+  /** @type {AssertConfig} */ (config).module.rules.push(
+    {
+      test: /\.(js|ts|tsx)$/,
+      // Apply "exclude" only if your dependencies **do not use** Griffel
+      // exclude: /node_modules/,
+      use: {
+        loader: GriffelCSSExtractionPlugin.loader,
+      },
+    },
+    {
+      test: /\.css$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader'],
+    },
+  );
+  /** @type {AssertConfig} */ (config).plugins.push(new GriffelCSSExtractionPlugin(), new MiniCssExtractPlugin());
+  // config.resolve.extensions = ['.raw.js', '...']; // Add Griffel AOT extraction for better bundle size measurements
+}
 
 /** @type {import('monosize').MonoSizeConfig} */
 const config = {
   ...baseConfig,
   bundler: webpackBundler((config) => {
+    config.resolve = config.resolve ?? {};
     config.module = config.module ?? {};
     config.module.rules = config.module.rules ?? [];
+    config.plugins = config.plugins ?? [];
+
+    if (enableGriffelExtraction) {
+      applyGriffelExtraction(config);
+    }
 
     // Add loader for font files
     config.module.rules.push({
