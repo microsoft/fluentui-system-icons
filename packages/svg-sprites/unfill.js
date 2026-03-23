0a60ea089c08b24b706db843280f394f2e3f74cf
Martin Hochel <hochelmartin@gmail.com>
2026-03-23 15:58:18 +0100

    fix: │ resolve all npm audit high severity vulnerabilities

--- packages/svg-sprites/unfill.js
+++ packages/svg-sprites/unfill.js
@@ -1,17 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-// The 'replace' module doesn't work on Windows command line.
-const r = require('replace');
+const { replaceInFiles } = require('../../importer/replace-in-files');
 const argv = require('yargs').argv;
 
-const path = argv.path;
-
-r({
-  regex: /\sfill="(none|#212121)"/g,
+replaceInFiles({
+  search: /\sfill="(none|#212121)"/g,
   replacement: '',
-  paths: [path],
+  targets: [argv.path],
   recursive: true,
   include: '*.svg',
-  silent: true,
+  quiet: true,
 });
