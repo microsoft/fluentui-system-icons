cf9730e9105fe8e187e7714cc7905f9f0c537572
Martin Hochel <hochelmartin@gmail.com>
2026-03-23 14:50:15 +0100

    refactor(react-icons): merge static assets under one .d.ts

--- packages/react-icons/src/assets.d.ts
+++ packages/react-icons/src/assets.d.ts
@@ -1,3 +1,11 @@
+// SVG file types - for svg sprite
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+// Font file types - for icon fonts
+
 declare module '*.ttf' {
   const uri: string;
   export default uri;
