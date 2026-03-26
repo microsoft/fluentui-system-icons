5351dc6ee7b0090a07e160f4352abd0b6f1260fc
Martin Hochel <hochelmartin@gmail.com>
2026-03-26 18:54:17 +0100

    fix(docsite): remove node_module overrides and make it work on node 22

--- packages/docsite/.storybook/main.ts
+++ packages/docsite/.storybook/main.ts
@@ -1,14 +1,12 @@
-import { fileURLToPath } from 'node:url';
-import { dirname } from 'node:path';
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)', '../stories/**/*.mdx'],
 
-  addons: [getAbsolutePath('@storybook/addon-docs'), getAbsolutePath('@storybook/addon-webpack5-compiler-swc')],
+  addons: ['@storybook/addon-docs', '@storybook/addon-webpack5-compiler-swc'],
 
   framework: {
-    name: getAbsolutePath('@storybook/react-webpack5'),
+    name: '@storybook/react-webpack5',
     options: {},
   },
 
@@ -19,17 +17,6 @@ const config: StorybookConfig = {
   docs: {
     docsMode: true,
   },
-
-  webpackFinal: async (config) => {
-    const docsiteNodeModules = new URL('../node_modules', import.meta.url).pathname;
-    config.resolve = config.resolve || {};
-    config.resolve.modules = [docsiteNodeModules, ...(config.resolve.modules || ['node_modules'])];
-    return config;
-  },
 };
 
 export default config;
-
-function getAbsolutePath(value: string): any {
-  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)));
-}
