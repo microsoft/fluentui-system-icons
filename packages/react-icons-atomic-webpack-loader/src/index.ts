3ce397a1bd99a4b5f8f9c72edf12bf636fd6e3b1
viktorgenaev <viktorgenaev@microsoft.com>
2026-04-16 17:07:40 +0200

    fix: address review suggestions

--- packages/react-icons-atomic-webpack-loader/src/index.ts
+++ packages/react-icons-atomic-webpack-loader/src/index.ts
@@ -1,9 +1,6 @@
 import { transformSource } from './transform';
 import type { LoaderContext } from 'webpack';
 
-const REACT_ICONS_IMPORT_REGEX = /['"]@fluentui\/react-icons['";\s]/;
-const TS_EXT_REGEX = /\.[mc]?tsx?$/;
-
 export interface FluentIconsAtomicImportLoaderOptions {
   iconVariant?: 'svg' | 'fonts' | 'svg-sprite';
 }
@@ -14,18 +11,16 @@ export default function fluentIconsAtomicImportLoader(
 ): void {
   const { resourcePath } = this;
 
-  if (!REACT_ICONS_IMPORT_REGEX.test(sourceCode)) {
+  if (!sourceCode.includes('@fluentui/react-icons')) {
     return this.callback(null, sourceCode);
   }
 
   const { iconVariant = 'svg' } = this.getOptions();
-  const isTypescript = TS_EXT_REGEX.test(resourcePath);
 
   try {
-    const { code, map } = transformSource(sourceCode, { iconVariant, isTypescript, path: resourcePath });
+    const { code, map } = transformSource(sourceCode, { iconVariant, path: resourcePath });
     return this.callback(null, code, map);
   } catch {
-    this.emitWarning(new Error(`FluentIconsAtomicImportLoader: Failed to transform "${resourcePath}"`));
-    return this.callback(null, sourceCode);
+    return this.callback(new Error(`FluentIconsAtomicImportLoader: Failed to transform "${resourcePath}"`));
   }
 }
